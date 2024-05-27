ab7b95660b400518c555df29cd196dd6387087d1
qwfsdaf <xogh990@gmail.com>
2024-05-27 19:09:38 +0900

    토글바 추가

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { IoIosMenu } from "react-icons/io";
 import styles from '../Header/Header.module.css'
+import Sidebar from '../components/Sidebar';
 
 export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.logo}>Logo</div>
-            <button className={styles.button}><IoIosMenu /></button>
+            <Sidebar />
         </header>
     )
 }
