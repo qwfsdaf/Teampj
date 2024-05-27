1ad70338675475cea0c5fcf0f5d951e7a3259053
qwfsdaf <xogh990@gmail.com>
2024-05-28 00:53:11 +0900

    로고 홈 네비게이션추가

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import styles from '../Header/Header.module.css'
 import Sidebar from '../components/Sidebar';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+
+    const navigate = useNavigate();
+
+    const handleClick = (e) => {
+        e.preventDefault(); // 기본 이벤트를 방지합니다.
+
+        navigate('/');
+    };
+
     return (
         <header className={styles.header}>
-            <div className={styles.logo}>Logo</div>
+            <button onClick={handleClick} className={styles.logo}>Logo</button>
             <Sidebar />
         </header>
     )
