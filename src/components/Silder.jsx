94ab6d445f676fb43fad173617a7d2b3db72d62f
qwfsdaf <xogh990@gmail.com>
2024-05-27 17:42:51 +0900

    back 수정

--- src/components/Silder.jsx
+++ src/components/Silder.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Slider from "react-slick"
-import styles from "./Slider.module.css"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import test1 from '../Pages/test1.png'
-import test2 from '../Pages/test2.png'
-import test3 from '../Pages/test3.png'
-import test4 from '../Pages/test4.png'
+import React from 'react';
+import Slider from 'react-slick';
+import styles from './Slider.module.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import test1 from '../Pages/test1.png';
+import test2 from '../Pages/test2.png';
+import test3 from '../Pages/test3.png';
+import test4 from '../Pages/test4.png';
 
 function SimpleSlider() {
     const settings = {
@@ -16,26 +16,27 @@ function SimpleSlider() {
         slidesToShow: 2,
         slidesToScroll: 2
     };
+
     return (
-        <div className="slider-container">
+        <div className={styles.slidercontainer}>
             <Slider {...settings}>
-                <div className={styles.text}>
+                <div className={styles.myclosetdiv}>
                     <img className={styles.text} src={test1} alt='test1' />
                 </div>
-                <div className={styles.text}>
-                    <img className={styles.text} src={test2} alt='test1' />
+                <div className={styles.myclosetdiv}>
+                    <img className={styles.text} src={test2} alt='test2' />
                 </div>
-                <div className={styles.text}>
-                    <img className={styles.text} src={test3} alt='test1' />
+                <div className={styles.myclosetdiv}>
+                    <img className={styles.text} src={test3} alt='test3' />
                 </div>
-                <div className={styles.text}>
-                    <img className={styles.text} src={test4} alt='test1' />
+                <div className={styles.myclosetdiv}>
+                    <img className={styles.text} src={test4} alt='test4' />
                 </div>
-                <div>
-                    <h3>5</h3>
+                <div className={styles.myclosetdiv}>
+                    <h3 className={styles.text}>5</h3>
                 </div>
-                <div>
-                    <h3>6</h3>
+                <div className={styles.myclosetdiv}>
+                    <h3 className={styles.text}>6</h3>
                 </div>
             </Slider>
         </div>
