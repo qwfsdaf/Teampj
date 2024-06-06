250860f071903cb413e912f5a193ca4c8872ef64
qwfsdaf <xogh990@gmail.com>
2024-06-06 20:25:51 +0900

    dasd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Idform from './Idform/Idform.jsx'
-import Login from './Signup/Login.jsx'
-import NotFound from './components/NotFound.jsx'
-import Idfind from './find/Idfind.jsx'
-import Pwmodify from './find/Pwmodify.jsx'
-import ChatbotMain from './Pages/Chatbotmain.jsx'
-import Imageanalysis from './Pages/Imageanalysis.jsx'
-import Home from './Home/Home.jsx'
-import Mypage from './Pages/Mypage.jsx'
-import Community from './Community/Community.jsx'
-import Communitywrite from './Community/Communitywrite.jsx'
+import React from 'react';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import Idform from './Idform/Idform.jsx';
+import Login from './Signup/Login.jsx';
+import NotFound from './components/NotFound.jsx';
+import Idfind from './find/Idfind.jsx';
+import Pwmodify from './find/Pwmodify.jsx';
+import ChatbotMain from './Pages/Chatbotmain.jsx';
+import Imageanalysis from './Pages/Imageanalysis.jsx';
+import Home from './Home/Home.jsx';
+import Mypage from './Pages/Mypage.jsx';
+import Community from './Community/Community.jsx';
+import Communitywrite from './Community/Communitywrite.jsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/Login',
     element: <Login />,
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
     path: '/communitywrite',
     element: <Communitywrite />
   }
-])
+]);
 
 export default function App() {
   return <RouterProvider router={router} />;
